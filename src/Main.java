1925f124d592ab38355d9b69933a9852a7cd7496
Justin Chong <jchong8630@bths.edu>
2023-02-09 21:58:59 -0500

    Finished 4th Degree

--- src/Main.java
+++ src/Main.java
@@ -8,88 +8,151 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         ArrayList<SimpleMovie> movies = MovieDatabaseBuilder.getMovieDB("src/movie_data");
-//        for (SimpleMovie movie : movies) {
-//            System.out.println(movie);
-//        }
-//        System.out.println("Number of movies: " + movies.size());
         ArrayList<String> kbActors = MovieDatabaseBuilder.getActors("src/ActorNames");
         ArrayList<SimpleMovie> kbActorsMovies =  MovieDatabaseBuilder.getMovieDB("src/kbActorMovies");
-//        ArrayList<String> kbActors2ndDegree= MovieDatabaseBuilder.getActors("src/2Degree");
-//        try {
-//            File f = new File("src/2Degree");
-//            f.createNewFile();
-//            FileWriter fw = new FileWriter(f);
-//            for (String n : kbActorsInMoviesActors) {
-//                fw.write(n + "\n");
-//            }
-//            fw.close();
-//        }
-//        catch (IOException ioe) {
-//            System.out.println("Writing file failed");
-//            System.out.println(ioe);
-//        }
-
-//
-//        try {
-//            File f = new File("src/kbActorMovies");
-//            f.createNewFile();
-//            FileWriter fw = new FileWriter(f);
-//            for (SimpleMovie n : kbActorsMovies) {
-//                fw.write(n.getTitle() + "---" + n.getActorsStr() + "\n");
-//            }
-//            fw.close();
-//        }
-//        catch (IOException ioe) {
-//            System.out.println("Writing file failed");
-//            System.out.println(ioe);
-//        }
+        ArrayList<SimpleMovie> kbMovies = MovieDatabaseBuilder.getMovieDB("src/kbMovies");
         ArrayList<SimpleMovie> actorMovies = new ArrayList<>();
         ArrayList<String> actorMoviesActors = new ArrayList<>();
+        ArrayList<SimpleMovie> actorMoviesActorsMovies = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         System.out.print("Enter an actor's name: ");
         String actor = s.nextLine();
 
-        for (int i = 0; i < movies.size(); i++){
-            if (movies.get(i).getActors().contains(actor)){
-                actorMovies.add(movies.get(i));
+        for (SimpleMovie value : movies) {
+            if (value.getActors().contains(actor)) {
+                actorMovies.add(value);
             }
         }
-        for (int i = 0; i < actorMovies.size(); i++){
-            for (int j = 0; j < actorMovies.get(i).getActors().size(); j++){
-                String actorIn = actorMovies.get(i).getActors().get(j);
-                if (!(actorMoviesActors.contains(actorIn))){
+        for (SimpleMovie simpleMovie : actorMovies) {
+            for (int j = 0; j < simpleMovie.getActors().size(); j++) {
+                String actorIn = simpleMovie.getActors().get(j);
+                if (!(actorMoviesActors.contains(actorIn))) {
                     actorMoviesActors.add(actorIn);
                 }
             }
         }
 
-        boolean checker = false;
-        if (kbActors.contains(actor)){
-            System.out.println("Bacon Number: 1");
-            checker = true;
-        }
-        if (checker != true){
-            for (int i = 0; i < kbActorsMovies.size(); i++){
-                if (kbActorsMovies.get(i).getActors().contains(actor)){
-                    System.out.println("Bacon Number: 2");
-                    System.out.println(kbActorsMovies.get(i));
-                    checker = true;
+        for (SimpleMovie movie : movies) {
+            for (String actorMoviesActor : actorMoviesActors) {
+                if (movie.getActors().contains(actorMoviesActor)) {
+                    actorMoviesActorsMovies.add(movie);
                     break;
                 }
             }
         }
-        if (checker != true){
-            for (int i = 0; i < actorMoviesActors.size(); i++){
-                for (int j = 0; j < kbActorsMovies.size(); j++){
-                    if (kbActorsMovies.get(j).getActors().contains(actorMoviesActors.get(i))){
+        boolean checker = false;
+        if (kbActors.contains(actor)){
+            System.out.println("Bacon Number: 1");
+            for (SimpleMovie kbMovie : kbMovies) {
+                if (kbMovie.getActors().contains(actor)) {
+                    System.out.println(actor + " --> " + kbMovie.getTitle() + "  --> Kevin Bacon");
+                    break;
+                }
+            }
+            checker = true;
+        }
+        if (!checker){
+            for (SimpleMovie kbActorsMovie : kbActorsMovies) {
+                if (kbActorsMovie.getActors().contains(actor)) {
+                    System.out.println("Bacon Number: 2");
+                    String mainMovie = kbActorsMovie.getTitle();
+                    checker = true;
+                    for (int k = 0; k < kbActorsMovie.getActors().size(); k++) {
+                        String nextActor = kbActorsMovie.getActors().get(k);
+                        if (kbActors.contains(nextActor)) {
+                            for (SimpleMovie kbMovie : kbMovies) {
+                                if (kbMovie.getActors().contains(nextActor)) {
+                                    String nextMovie = kbMovie.getTitle();
+                                    System.out.println(nextMovie);
+                                    System.out.println(actor + " --> " + mainMovie + " --> " + nextActor + " --> " + nextMovie + "  --> Kevin Bacon");
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        if (!checker){
+            for (String actorMoviesActor : actorMoviesActors) {
+                for (SimpleMovie kbActorsMovie : kbActorsMovies) {
+                    if (kbActorsMovie.getActors().contains(actorMoviesActor)) {
+                        String mainMovie = "";
                         System.out.println("Bacon Number: 3");
                         checker = true;
-                        System.out.println(kbActorsMovies.get(j));
-                        System.out.println(actorMoviesActors.get(i));
+                        for (SimpleMovie actorMovie : actorMovies) {
+                            if (actorMovie.getActors().contains(actorMoviesActor)) {
+                                mainMovie = actorMovie.getTitle();
+                            }
+                        }
+                        for (int k = 0; k < kbActorsMovie.getActors().size(); k++) {
+                            if (kbActors.contains(kbActorsMovie.getActors().get(k))) {
+                                String actorTwo = kbActorsMovie.getActors().get(k);
+                                for (SimpleMovie kbMovie : kbMovies) {
+                                    if (kbMovie.getActors().contains(actorTwo)) {
+                                        String movieTwo = kbMovie.getTitle();
+                                        System.out.println(actor + " --> " + mainMovie + " --> " + actorMoviesActor + " --> " + kbActorsMovie.getTitle() + " --> " + actorTwo + " --> " + movieTwo + "  --> Kevin Bacon");
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        break;
+
+                    }
+                }
+                if (checker) {
+                    break;
+                }
+            }
+        }
+        if (!checker){
+            String firstActor = "";
+            System.out.println("Bacon Number: 4");
+            for (SimpleMovie actorMoviesActorsMovie : actorMoviesActorsMovies) {
+                for (int j = 0; j < kbActorsMovies.size(); j++) {
+                    for (int k = 0; k < actorMoviesActorsMovie.getActors().size(); k++) {
+                        if (kbActorsMovies.get(j).getActors().contains((actorMoviesActorsMovie.getActors().get(k)))) {
+                            firstActor = actorMoviesActorsMovie.getActors().get(k);
+                            SimpleMovie mainMovie = null;
+                            String ogActor = "";
+                            for (String actorMoviesActor : actorMoviesActors) {
+                                if (actorMoviesActorsMovie.getActors().contains(actorMoviesActor)) {
+                                    ogActor = actorMoviesActor;
+                                    for (SimpleMovie actorMovie : actorMovies) {
+                                        if (actorMovie.getActors().contains(ogActor)) {
+                                            mainMovie = actorMovie;
+                                        }
+                                    }
+                                }
+                            }
+                            for (SimpleMovie kbActorsMovie : kbActorsMovies) {
+                                if (kbActorsMovie.getActors().contains(firstActor)) {
+                                    for (String kbActor : kbActors) {
+                                        if (kbActorsMovie.getActors().contains(kbActor)) {
+                                            for (SimpleMovie kbMovie : kbMovies) {
+                                                if (kbMovie.getActors().contains(kbActor)) {
+                                                    System.out.println(actor + " --> " + mainMovie.getTitle() + " --> " + ogActor + " --> " + actorMoviesActorsMovie.getTitle() + " --> " + firstActor + " --> " + kbActorsMovie.getTitle() + " --> " + kbActor + " --> " + kbMovie.getTitle() + "  --> Kevin Bacon");
+                                                    break;
+                                                }
+                                            }
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    if (firstActor != "") {
                         break;
                     }
                 }
-                if (checker == true){
+                if (firstActor != "") {
                     break;
                 }
             }
